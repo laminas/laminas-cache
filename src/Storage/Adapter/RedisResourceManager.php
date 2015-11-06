e325e3b7f9ffbcd47aa7b8969f674b84ab7cb8dd
Marc Bennewitz <marc@mabe.berlin>
2015-11-06 21:55:20 +0100

    fix #18: Redis::getCapabilities() error when not yet initialized  - fixed `RedisResourceManager::getMajorVersion()`    by initialize the resource before accessing version info  - Get the resource manager/id from options    instead of access `$this->resourceManager` which could be still not initialized  - flush test data in `tearDown()` instead of `setUp()`    with this change the bug gets reflected by `testGetCapabilities()`    and it makes sure that all test data gets removed after

--- src/Storage/Adapter/RedisResourceManager.php
+++ src/Storage/Adapter/RedisResourceManager.php
@@ -47,12 +47,10 @@ class RedisResourceManager
      */
     public function getMajorVersion($id)
     {
-        if (!$this->hasResource($id)) {
-            throw new Exception\RuntimeException("No resource with id '{$id}'");
-        }
+        // check resource id and initialize the resource
+        $this->getResource($id);
 
-        $resource = & $this->resources[$id];
-        return (int) $resource['version'];
+        return (int) $this->resources[$id]['version'];
     }
 
     /**
