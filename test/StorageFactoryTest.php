70d9ddc92e3dc58054ee0e937c475df019a6dbc9
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:19:21 -0600

    Rewriting as Laminas Project package

--- test/StorageFactoryTest.php
+++ test/StorageFactoryTest.php
@@ -1,22 +1,20 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Cache
+ * @see       https://github.com/laminas/laminas-cache for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-cache/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-cache/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Cache;
+namespace LaminasTest\Cache;
 
-use Zend\Cache;
+use Laminas\Cache;
 
 /**
- * @category   Zend
- * @package    Zend_Cache
+ * @category   Laminas
+ * @package    Laminas_Cache
  * @subpackage UnitTests
- * @group      Zend_Cache
+ * @group      Laminas_Cache
  */
 class StorageFactoryTest extends \PHPUnit_Framework_TestCase
 {
@@ -36,7 +34,7 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
     public function testDefaultAdapterPluginManager()
     {
         $adapters = Cache\StorageFactory::getAdapterPluginManager();
-        $this->assertInstanceOf('Zend\Cache\Storage\AdapterPluginManager', $adapters);
+        $this->assertInstanceOf('Laminas\Cache\Storage\AdapterPluginManager', $adapters);
     }
 
     public function testChangeAdapterPluginManager()
@@ -49,10 +47,10 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
     public function testAdapterFactory()
     {
         $adapter1 = Cache\StorageFactory::adapterFactory('Memory');
-        $this->assertInstanceOf('Zend\Cache\Storage\Adapter\Memory', $adapter1);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Adapter\Memory', $adapter1);
 
         $adapter2 = Cache\StorageFactory::adapterFactory('Memory');
-        $this->assertInstanceOf('Zend\Cache\Storage\Adapter\Memory', $adapter2);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Adapter\Memory', $adapter2);
 
         $this->assertNotSame($adapter1, $adapter2);
     }
@@ -60,7 +58,7 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
     public function testDefaultPluginManager()
     {
         $manager = Cache\StorageFactory::getPluginManager();
-        $this->assertInstanceOf('Zend\Cache\Storage\PluginManager', $manager);
+        $this->assertInstanceOf('Laminas\Cache\Storage\PluginManager', $manager);
     }
 
     public function testChangePluginManager()
@@ -73,10 +71,10 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
     public function testPluginFactory()
     {
         $plugin1 = Cache\StorageFactory::pluginFactory('Serializer');
-        $this->assertInstanceOf('Zend\Cache\Storage\Plugin\Serializer', $plugin1);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Plugin\Serializer', $plugin1);
 
         $plugin2 = Cache\StorageFactory::pluginFactory('Serializer');
-        $this->assertInstanceOf('Zend\Cache\Storage\Plugin\Serializer', $plugin2);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Plugin\Serializer', $plugin2);
 
         $this->assertNotSame($plugin1, $plugin2);
     }
@@ -86,7 +84,7 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
         $cache = Cache\StorageFactory::factory(array(
             'adapter' => 'Memory',
         ));
-        $this->assertInstanceOf('Zend\Cache\Storage\Adapter\Memory', $cache);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Adapter\Memory', $cache);
     }
 
     public function testFactoryAdapterAsArray()
@@ -96,7 +94,7 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
                 'name' => 'Memory',
             )
         ));
-        $this->assertInstanceOf('Zend\Cache\Storage\Adapter\Memory', $cache);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Adapter\Memory', $cache);
     }
 
     public function testFactoryWithPlugins()
@@ -110,12 +108,12 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
         ));
 
         // test adapter
-        $this->assertInstanceOf('Zend\Cache\Storage\Adapter\Memory', $cache);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Adapter\Memory', $cache);
 
         // test plugin structure
         $i = 0;
         foreach ($cache->getPluginRegistry() as $plugin) {
-            $this->assertInstanceOf('Zend\Cache\Storage\Plugin\\' . $plugins[$i++], $plugin);
+            $this->assertInstanceOf('Laminas\Cache\Storage\Plugin\\' . $plugins[$i++], $plugin);
         }
     }
 
@@ -154,7 +152,7 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
         $storage = Cache\StorageFactory::factory($factory);
 
         // test adapter
-        $this->assertInstanceOf('Zend\Cache\Storage\Adapter\\' . $factory['adapter']['name'], $storage);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Adapter\\' . $factory['adapter']['name'], $storage);
         $this->assertEquals(123, $storage->getOptions()->getTtl());
         $this->assertEquals('test', $storage->getOptions()->getNamespace());
 
@@ -164,17 +162,17 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
             // test plugin options
             $pluginClass = get_class($plugin);
             switch ($pluginClass) {
-                case 'Zend\Cache\Storage\Plugin\ClearExpiredByFactor':
+                case 'Laminas\Cache\Storage\Plugin\ClearExpiredByFactor':
                     $this->assertSame(
                         $factory['plugins']['ClearExpiredByFactor']['clearing_factor'],
                         $plugin->getOptions()->getClearingFactor()
                     );
                     break;
 
-                case 'Zend\Cache\Storage\Plugin\Serializer':
+                case 'Laminas\Cache\Storage\Plugin\Serializer':
                     break;
 
-                case 'Zend\Cache\Storage\Plugin\IgnoreUserAbort':
+                case 'Laminas\Cache\Storage\Plugin\IgnoreUserAbort':
                     $this->assertFalse($plugin->getOptions()->getExitOnAbort());
                     break;
 
