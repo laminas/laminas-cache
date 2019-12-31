838eb5ef579cdebc9fd9327274793297f3b09150
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:19:02 -0600

    Rewriting as Laminas Project package

--- src/Pattern/AbstractPattern.php
+++ src/Pattern/AbstractPattern.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-cache for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-cache/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-cache/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Cache\Pattern;
+namespace Laminas\Cache\Pattern;
 
+use Laminas\Cache\Exception;
 use Traversable;
-use Zend\Cache\Exception;
 
 abstract class AbstractPattern implements PatternInterface
 {
