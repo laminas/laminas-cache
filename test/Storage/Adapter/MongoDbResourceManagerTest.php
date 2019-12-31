5607fb25bf427088385ac27b7747b26542f7a19a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:21:27 -0600

    Rewriting as Laminas Project package

--- test/Storage/Adapter/MongoDbResourceManagerTest.php
+++ test/Storage/Adapter/MongoDbResourceManagerTest.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-cache for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-cache/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-cache/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Cache\Storage\Adapter;
+namespace LaminasTest\Cache\Storage\Adapter;
 
-use Zend\Cache\Storage\Adapter\MongoDbResourceManager;
+use Laminas\Cache\Storage\Adapter\MongoDbResourceManager;
 
 /**
- * @group      Zend_Cache
+ * @group      Laminas_Cache
  */
 class MongoDbResourceManagerTest extends \PHPUnit_Framework_TestCase
 {
@@ -20,8 +19,8 @@ class MongoDbResourceManagerTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        if (!defined('TESTS_ZEND_CACHE_MONGODB_ENABLED') || !TESTS_ZEND_CACHE_MONGODB_ENABLED) {
-            $this->markTestSkipped("Skipped by TestConfiguration (TESTS_ZEND_CACHE_MONGODB_ENABLED)");
+        if (!defined('TESTS_LAMINAS_CACHE_MONGODB_ENABLED') || !TESTS_LAMINAS_CACHE_MONGODB_ENABLED) {
+            $this->markTestSkipped("Skipped by TestConfiguration (TESTS_LAMINAS_CACHE_MONGODB_ENABLED)");
         }
 
         if (!extension_loaded('mongo') || !class_exists('\Mongo') || !class_exists('\MongoClient')) {
@@ -39,8 +38,8 @@ class MongoDbResourceManagerTest extends \PHPUnit_Framework_TestCase
         $id = 'foo';
 
         $clientClass = (version_compare(phpversion('mongo'), '1.3.0', '<')) ? '\Mongo' : '\MongoClient';
-        $client = new $clientClass(TESTS_ZEND_CACHE_MONGODB_CONNECTSTRING);
-        $resource = $client->selectCollection(TESTS_ZEND_CACHE_MONGODB_DATABASE, TESTS_ZEND_CACHE_MONGODB_COLLECTION);
+        $client = new $clientClass(TESTS_LAMINAS_CACHE_MONGODB_CONNECTSTRING);
+        $resource = $client->selectCollection(TESTS_LAMINAS_CACHE_MONGODB_DATABASE, TESTS_LAMINAS_CACHE_MONGODB_COLLECTION);
 
         $this->object->setResource($id, $resource);
 
@@ -64,7 +63,7 @@ class MongoDbResourceManagerTest extends \PHPUnit_Framework_TestCase
         $id = 'foo';
         $resource = new \stdClass();
 
-        $this->setExpectedException('Zend\Cache\Exception\InvalidArgumentException');
+        $this->setExpectedException('Laminas\Cache\Exception\InvalidArgumentException');
         $this->object->setResource($id, $resource);
     }
 
@@ -90,7 +89,7 @@ class MongoDbResourceManagerTest extends \PHPUnit_Framework_TestCase
 
         $this->assertFalse($this->object->hasResource($id));
 
-        $this->setExpectedException('Zend\Cache\Exception\RuntimeException');
+        $this->setExpectedException('Laminas\Cache\Exception\RuntimeException');
         $this->object->getResource($id);
     }
 
@@ -99,8 +98,8 @@ class MongoDbResourceManagerTest extends \PHPUnit_Framework_TestCase
         $id = 'foo';
 
         $clientClass = (version_compare(phpversion('mongo'), '1.3.0', '<')) ? '\Mongo' : '\MongoClient';
-        $client = new $clientClass(TESTS_ZEND_CACHE_MONGODB_CONNECTSTRING);
-        $resource = $client->selectCollection(TESTS_ZEND_CACHE_MONGODB_DATABASE, TESTS_ZEND_CACHE_MONGODB_COLLECTION);
+        $client = new $clientClass(TESTS_LAMINAS_CACHE_MONGODB_CONNECTSTRING);
+        $resource = $client->selectCollection(TESTS_LAMINAS_CACHE_MONGODB_DATABASE, TESTS_LAMINAS_CACHE_MONGODB_COLLECTION);
 
         $this->object->setResource($id, $resource);
 
@@ -110,10 +109,10 @@ class MongoDbResourceManagerTest extends \PHPUnit_Framework_TestCase
     public function testGetResourceNewResource()
     {
         $id                = 'foo';
-        $server            = TESTS_ZEND_CACHE_MONGODB_CONNECTSTRING;
+        $server            = TESTS_LAMINAS_CACHE_MONGODB_CONNECTSTRING;
         $connectionOptions = array('connectTimeoutMS' => 5);
-        $database          = TESTS_ZEND_CACHE_MONGODB_DATABASE;
-        $collection        = TESTS_ZEND_CACHE_MONGODB_COLLECTION;
+        $database          = TESTS_LAMINAS_CACHE_MONGODB_DATABASE;
+        $collection        = TESTS_LAMINAS_CACHE_MONGODB_COLLECTION;
 
         $this->object->setServer($id, $server);
         $this->object->setConnectionOptions($id, $connectionOptions);
@@ -128,15 +127,15 @@ class MongoDbResourceManagerTest extends \PHPUnit_Framework_TestCase
         $id                = 'foo';
         $server            = 'mongodb://unknown.unknown';
         $connectionOptions = array('connectTimeoutMS' => 5);
-        $database          = TESTS_ZEND_CACHE_MONGODB_DATABASE;
-        $collection        = TESTS_ZEND_CACHE_MONGODB_COLLECTION;
+        $database          = TESTS_LAMINAS_CACHE_MONGODB_DATABASE;
+        $collection        = TESTS_LAMINAS_CACHE_MONGODB_COLLECTION;
 
         $this->object->setServer($id, $server);
         $this->object->setConnectionOptions($id, $connectionOptions);
         $this->object->setDatabase($id, $database);
         $this->object->setCollection($id, $collection);
 
-        $this->setExpectedException('Zend\Cache\Exception\RuntimeException');
+        $this->setExpectedException('Laminas\Cache\Exception\RuntimeException');
         $this->object->getResource($id);
     }
 }
--- test/StorageFactoryTest.php
+++ test/StorageFactoryTest.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-cache for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-cache/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-cache/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Cache;
+namespace LaminasTest\Cache;
 
-use Zend\Cache;
+use Laminas\Cache;
 
 /**
- * @group      Zend_Cache
+ * @group      Laminas_Cache
  */
 class StorageFactoryTest extends \PHPUnit_Framework_TestCase
 {
@@ -31,7 +30,7 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
     public function testDefaultAdapterPluginManager()
     {
         $adapters = Cache\StorageFactory::getAdapterPluginManager();
-        $this->assertInstanceOf('Zend\Cache\Storage\AdapterPluginManager', $adapters);
+        $this->assertInstanceOf('Laminas\Cache\Storage\AdapterPluginManager', $adapters);
     }
 
     public function testChangeAdapterPluginManager()
@@ -44,10 +43,10 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
     public function testAdapterFactory()
     {
         $adapter1 = Cache\StorageFactory::adapterFactory('Memory');
-        $this->assertInstanceOf('Zend\Cache\Storage\Adapter\Memory', $adapter1);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Adapter\Memory', $adapter1);
 
         $adapter2 = Cache\StorageFactory::adapterFactory('Memory');
-        $this->assertInstanceOf('Zend\Cache\Storage\Adapter\Memory', $adapter2);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Adapter\Memory', $adapter2);
 
         $this->assertNotSame($adapter1, $adapter2);
     }
@@ -55,7 +54,7 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
     public function testDefaultPluginManager()
     {
         $manager = Cache\StorageFactory::getPluginManager();
-        $this->assertInstanceOf('Zend\Cache\Storage\PluginManager', $manager);
+        $this->assertInstanceOf('Laminas\Cache\Storage\PluginManager', $manager);
     }
 
     public function testChangePluginManager()
@@ -68,10 +67,10 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
     public function testPluginFactory()
     {
         $plugin1 = Cache\StorageFactory::pluginFactory('Serializer');
-        $this->assertInstanceOf('Zend\Cache\Storage\Plugin\Serializer', $plugin1);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Plugin\Serializer', $plugin1);
 
         $plugin2 = Cache\StorageFactory::pluginFactory('Serializer');
-        $this->assertInstanceOf('Zend\Cache\Storage\Plugin\Serializer', $plugin2);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Plugin\Serializer', $plugin2);
 
         $this->assertNotSame($plugin1, $plugin2);
     }
@@ -81,7 +80,7 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
         $cache = Cache\StorageFactory::factory(array(
             'adapter' => 'Memory',
         ));
-        $this->assertInstanceOf('Zend\Cache\Storage\Adapter\Memory', $cache);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Adapter\Memory', $cache);
     }
 
     /**
@@ -96,7 +95,7 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
             ),
         ));
 
-        $this->assertInstanceOf('Zend\Cache\Storage\Adapter\Memory', $cache);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Adapter\Memory', $cache);
         $this->assertSame('test', $cache->getOptions()->getNamespace());
     }
 
@@ -107,7 +106,7 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
                 'name' => 'Memory',
             )
         ));
-        $this->assertInstanceOf('Zend\Cache\Storage\Adapter\Memory', $cache);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Adapter\Memory', $cache);
     }
 
     public function testFactoryWithPlugins()
@@ -121,19 +120,19 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
         ));
 
         // test adapter
-        $this->assertInstanceOf('Zend\Cache\Storage\Adapter\Memory', $cache);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Adapter\Memory', $cache);
 
         // test plugin structure
         $i = 0;
         foreach ($cache->getPluginRegistry() as $plugin) {
-            $this->assertInstanceOf('Zend\Cache\Storage\Plugin\\' . $plugins[$i++], $plugin);
+            $this->assertInstanceOf('Laminas\Cache\Storage\Plugin\\' . $plugins[$i++], $plugin);
         }
     }
 
     public function testFactoryInstantiateAdapterWithPluginsWithoutEventsCapableInterfaceThrowsException()
     {
         // The BlackHole adapter doesn't implement EventsCapableInterface
-        $this->setExpectedException('Zend\Cache\Exception\RuntimeException');
+        $this->setExpectedException('Laminas\Cache\Exception\RuntimeException');
         Cache\StorageFactory::factory(array(
             'adapter' => 'blackhole',
             'plugins' => array('Serializer'),
@@ -175,7 +174,7 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
         $storage = Cache\StorageFactory::factory($factory);
 
         // test adapter
-        $this->assertInstanceOf('Zend\Cache\Storage\Adapter\\' . $factory['adapter']['name'], $storage);
+        $this->assertInstanceOf('Laminas\Cache\Storage\Adapter\\' . $factory['adapter']['name'], $storage);
         $this->assertEquals(123, $storage->getOptions()->getTtl());
         $this->assertEquals('test', $storage->getOptions()->getNamespace());
 
@@ -184,17 +183,17 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase
             // test plugin options
             $pluginClass = get_class($plugin);
             switch ($pluginClass) {
-                case 'Zend\Cache\Storage\Plugin\ClearExpiredByFactor':
+                case 'Laminas\Cache\Storage\Plugin\ClearExpiredByFactor':
                     $this->assertSame(
                         $factory['plugins']['ClearExpiredByFactor']['clearing_factor'],
                         $plugin->getOptions()->getClearingFactor()
                     );
                     break;
 
-                case 'Zend\Cache\Storage\Plugin\Serializer':
+                case 'Laminas\Cache\Storage\Plugin\Serializer':
                     break;
 
-                case 'Zend\Cache\Storage\Plugin\IgnoreUserAbort':
+                case 'Laminas\Cache\Storage\Plugin\IgnoreUserAbort':
                     $this->assertFalse($plugin->getOptions()->getExitOnAbort());
                     break;
 
