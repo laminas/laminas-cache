4620a170a063c59fd1a1251caea568eeba916dd5
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:21:06 -0600

    Rewriting as Laminas Project package

--- test/Storage/Adapter/MongoDbTest.php
+++ test/Storage/Adapter/MongoDbTest.php
@@ -1,26 +1,25 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-cache for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-cache/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-cache/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Cache\Storage\Adapter;
+namespace LaminasTest\Cache\Storage\Adapter;
 
-use Zend\Cache\Storage\Adapter\MongoDb;
-use Zend\Cache\Storage\Adapter\MongoDbOptions;
+use Laminas\Cache\Storage\Adapter\MongoDb;
+use Laminas\Cache\Storage\Adapter\MongoDbOptions;
 
 /**
- * @group      Zend_Cache
+ * @group      Laminas_Cache
  */
 class MongoDbTest extends CommonAdapterTest
 {
     public function setUp()
     {
-        if (!defined('TESTS_ZEND_CACHE_MONGODB_ENABLED') || !TESTS_ZEND_CACHE_MONGODB_ENABLED) {
-            $this->markTestSkipped("Skipped by TestConfiguration (TESTS_ZEND_CACHE_MONGODB_ENABLED)");
+        if (!defined('TESTS_LAMINAS_CACHE_MONGODB_ENABLED') || !TESTS_LAMINAS_CACHE_MONGODB_ENABLED) {
+            $this->markTestSkipped("Skipped by TestConfiguration (TESTS_LAMINAS_CACHE_MONGODB_ENABLED)");
         }
 
         if (!extension_loaded('mongo') || !class_exists('\Mongo') || !class_exists('\MongoClient')) {
@@ -29,9 +28,9 @@ class MongoDbTest extends CommonAdapterTest
         }
 
         $this->_options = new MongoDbOptions(array(
-            'server'     => TESTS_ZEND_CACHE_MONGODB_CONNECTSTRING,
-            'database'   => TESTS_ZEND_CACHE_MONGODB_DATABASE,
-            'collection' => TESTS_ZEND_CACHE_MONGODB_COLLECTION,
+            'server'     => TESTS_LAMINAS_CACHE_MONGODB_CONNECTSTRING,
+            'database'   => TESTS_LAMINAS_CACHE_MONGODB_DATABASE,
+            'collection' => TESTS_LAMINAS_CACHE_MONGODB_COLLECTION,
         ));
 
         $this->_storage = new MongoDb();
@@ -53,12 +52,12 @@ class MongoDbTest extends CommonAdapterTest
     public function testSetOptionsNotMongoDbOptions()
     {
         $this->_storage->setOptions(array(
-            'server'     => TESTS_ZEND_CACHE_MONGODB_CONNECTSTRING,
-            'database'   => TESTS_ZEND_CACHE_MONGODB_DATABASE,
-            'collection' => TESTS_ZEND_CACHE_MONGODB_COLLECTION,
+            'server'     => TESTS_LAMINAS_CACHE_MONGODB_CONNECTSTRING,
+            'database'   => TESTS_LAMINAS_CACHE_MONGODB_DATABASE,
+            'collection' => TESTS_LAMINAS_CACHE_MONGODB_COLLECTION,
         ));
 
-        $this->assertInstanceOf('\Zend\Cache\Storage\Adapter\MongoDbOptions', $this->_storage->getOptions());
+        $this->assertInstanceOf('\Laminas\Cache\Storage\Adapter\MongoDbOptions', $this->_storage->getOptions());
     }
 
     public function testCachedItemsExpire()
