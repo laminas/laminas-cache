6af7d7c90bff8ce9b1523972c2449f5beb29d68e
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2020-11-19 19:05:05 +0100

    qa: add missing copyright doc
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/Storage/Adapter/TestAsset/AdapterOptionsWithPrioritizedOptions.php
+++ test/Storage/Adapter/TestAsset/AdapterOptionsWithPrioritizedOptions.php
@@ -1,14 +1,22 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * @see       https://github.com/laminas/laminas-cache-storage-adapter-test for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-cache-storage-adapter-test/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-cache-storage-adapter-test/blob/master/LICENSE.md New BSD License
+ */
+
 namespace LaminasTest\Cache\Storage\Adapter\TestAsset;
 
 use Laminas\Cache\Storage\Adapter\AdapterOptions;
 
 final class AdapterOptionsWithPrioritizedOptions extends AdapterOptions
 {
+    // @codingStandardsIgnoreStart
     protected $__prioritizedProperties__ = [
         'key_pattern',
         'namespace',
     ];
+    // @codingStandardsIgnoreEnd
 }
