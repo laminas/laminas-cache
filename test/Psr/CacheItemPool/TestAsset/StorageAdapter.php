bb2dbbe09d7a4fb95ab5eceeda719ca680d137fa
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2020-11-19 14:10:02 +0100

    qa: add missing license doc
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/Psr/CacheItemPool/TestAsset/StorageAdapter.php
+++ test/Psr/CacheItemPool/TestAsset/StorageAdapter.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * @see       https://github.com/laminas/laminas-cache for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-cache/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-cache/blob/master/LICENSE.md New BSD License
+ */
+
 namespace LaminasTest\Cache\Psr\CacheItemPool\TestAsset;
 
 use Laminas\Cache\Storage\Adapter\AbstractAdapter;
