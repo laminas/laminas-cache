0dcebb9f1f1e74577fee1d3f00b336de36698a45
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2021-07-27 18:00:37 +0200

    qa: add failing unit test
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/Service/DeprecatedSchemaDetectorTest.php
+++ test/Service/DeprecatedSchemaDetectorTest.php
@@ -19,6 +19,38 @@ final class DeprecatedSchemaDetectorTest extends TestCase
         $this->detector = new DeprecatedSchemaDetector();
     }
 
+    public function validStorageConfigurationSchemas(): Generator
+    {
+        yield 'adapter without plugins' => [
+            [
+                'adapter' => 'foo',
+            ],
+        ];
+
+        yield 'adapter with options' => [
+            [
+                'adapter' => 'foo',
+                'options' => [
+                    'bar' => 'baz',
+                ],
+            ],
+        ];
+
+        yield 'adapter with plugins' => [
+            [
+                'adapter' => 'foo',
+                'plugins' => [
+                    [
+                        'name' => 'bar',
+                        'options' => [
+                            'baz' => 'bat',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
     public function deprecatedStorageConfigurationSchemas(): Generator
     {
         yield 'adapter is not the alias or class-name of the adapter' => [
@@ -49,5 +81,14 @@ final class DeprecatedSchemaDetectorTest extends TestCase
     public function testWillProperlyDetectAllInvalidConfigurationSchemas(array $schema): void
     {
         self::assertTrue($this->detector->isDeprecatedStorageFactorySchema($schema));
+    }
+
+    /**
+     * @param array<string,mixed> $schema
+     * @dataProvider validStorageConfigurationSchemas
+     */
+    public function testWontDetectMigratedConfigurationSchemas(array $schema): void
+    {
+        self::assertFalse($this->detector->isDeprecatedStorageFactorySchema($schema));
     }
 }
