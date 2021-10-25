dfbbd7bc89e5d10141296082abbc7d3f4d10f915
Mario Costa <mariojr.rcosta@gmail.com>
2021-10-25 10:46:19 -0300

    bugix: adds Psr\SimpleCache\CacheException as return type hint of SimpleCacheDecorator::translateThrowable method
    Signed-off-by: Mario Costa <mariojr.rcosta@gmail.com>

--- src/Psr/SimpleCache/SimpleCacheDecorator.php
+++ src/Psr/SimpleCache/SimpleCacheDecorator.php
@@ -11,6 +11,7 @@ use Laminas\Cache\Storage\Capabilities;
 use Laminas\Cache\Storage\ClearByNamespaceInterface;
 use Laminas\Cache\Storage\FlushableInterface;
 use Laminas\Cache\Storage\StorageInterface;
+use Psr\SimpleCache\CacheException as PsrCacheException;
 use Psr\SimpleCache\CacheInterface as SimpleCacheInterface;
 use Throwable;
 use Traversable;
@@ -322,7 +323,7 @@ class SimpleCacheDecorator implements SimpleCacheInterface
         }
     }
 
-    private static function translateThrowable(Throwable $throwable): SimpleCacheException
+    private static function translateThrowable(Throwable $throwable): PsrCacheException
     {
         $exceptionClass = $throwable instanceof LaminasCacheInvalidArgumentException
             ? SimpleCacheInvalidArgumentException::class
