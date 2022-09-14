535badd6408599a13a555ad52c1eca28653c3c40
Gary Lockett <gary@creativecow.uk>
2022-09-14 18:26:21 +0100

    add declare(strict_types=1) only where they originally existed in src
    Signed-off-by: Gary Lockett <gary@creativecow.uk>

--- src/Service/StorageAdapterFactoryInterface.php
+++ src/Service/StorageAdapterFactoryInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Laminas\Cache\Service;
 
 use Laminas\Cache\Exception\InvalidArgumentException;
--- src/Service/StoragePluginFactory.php
+++ src/Service/StoragePluginFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Laminas\Cache\Service;
 
 use InvalidArgumentException as PhpInvalidArgumentException;
