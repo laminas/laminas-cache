81980ff75162d488adaae750ad423671aa140ff6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-11-08 22:20:11 +0700

    drop unused doc
    Signed-off-by: Abdul Malik Ikhsan <samsonasik@gmail.com>

--- src/Psr/CacheItemPool/CacheItem.php
+++ src/Psr/CacheItemPool/CacheItem.php
@@ -35,9 +35,6 @@ final class CacheItem implements CacheItemInterface
     private ClockInterface $clock;
 
     public function __construct(
-        /**
-         * Cache key
-         */
         private string $key,
         mixed $value,
         /**
